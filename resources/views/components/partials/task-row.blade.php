6c6b7fc0463ea5b39609dfb19df1ced880cd89ea
Delia Paternina <delia.paternina@gmail.com>
2025-03-24 18:52:30 +0000

    fixed tags layout

--- resources/views/components/partials/task-row.blade.php
+++ resources/views/components/partials/task-row.blade.php
@@ -1,8 +1,8 @@
 @props([
-'task' => null,
+    'task' => null,
 ])
 
-<div class="w-full flex py-2 border-b border-gray-100 items-center" x-data="{ showTagInput: false }" x-init="console.log('Alpine initialized', showTagInput)">
+<div class="w-full flex py-2 border-b border-gray-100 items-center min-h-[50px]" x-data="{ showTagInput: false }">
     <div class="w-2/12 flex items-center {{ $task?->complete ? 'line-through' : '' }}">
         {{ $task?->name }}
     </div>
@@ -12,32 +12,34 @@
     </div>
 
     <div class="w-4/12 flex items-center justify-start space-x-2 min-h-[50px]">
-        <x-elements.link-button class="w-[110px]" href="{{ route('tasks.complete', ['task' => $task]) }}">
-            {{ $task?->complete ? 'Pending' : 'Complete' }}
-        </x-elements.link-button>
-        <x-elements.link-button class="w-[110px]" href="{{ route('tasks.edit', ['task' => $task]) }}">
-            Edit
-        </x-elements.link-button>
-        <x-elements.link-button-danger class="w-[110px]" href="{{ route('tasks.destroy', ['task' => $task]) }}">
-            Delete
-        </x-elements.link-button-danger>
+        <div x-show="!showTagInput" class="flex space-x-2">
+            <x-elements.link-button class="w-[110px]" href="{{ route('tasks.complete', ['task' => $task]) }}">
+                {{ $task?->complete ? 'Pending' : 'Complete' }}
+            </x-elements.link-button>
+            <x-elements.link-button class="w-[110px]" href="{{ route('tasks.edit', ['task' => $task]) }}">
+                Edit
+            </x-elements.link-button>
+            <x-elements.link-button-danger class="w-[110px]" href="{{ route('tasks.destroy', ['task' => $task]) }}">
+                Delete
+            </x-elements.link-button-danger>
+        </div>
     </div>
 
-    <div class="w-5/12 flex justify-end">
-        @if($task->tags->isNotEmpty())
-        @foreach($task->tags as $tag)
-        <div class="group relarive inline-flex items-center">
-            <span class="inline-block bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-xs mr-2">
-                {{ $tag->name }}
-                <x-elements.remove-tag href="{{ route('tasks.tags.remove', ['task' => $task, 'tag' => $tag]) }}">
-                    x
-                </x-elements.remove-tag>
-            </span>
-        </div>
-        @endforeach
-        @endif
+    <div class="w-5/12 flex justify-end items-center min-h-[50px]">
 
         <div x-show="!showTagInput" style="display: none;">
+        @if($task->tags->isNotEmpty())
+            @foreach($task->tags as $tag)
+                <div class="group relarive inline-flex items-center">
+                    <span class="inline-block bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-xs mr-2">
+                        {{ $tag->name }}
+                        <x-elements.remove-tag href="{{ route('tasks.tags.remove', ['task' => $task, 'tag' => $tag]) }}">
+                            x
+                        </x-elements.remove-tag>
+                    </span>
+                </div>
+            @endforeach
+        @endif
             <x-elements.link-button
                 type="button"
                 x-on:click="showTagInput = true">
@@ -57,7 +59,10 @@
                 <x-elements.primary-button type="submit">
                     Add
                 </x-elements.primary-button>
-                <button type="button" class="bg-gray-500 text-white px-4 py-2 rounded" @click.prevent="showTagInput = false">
+                <button 
+                    type="button" 
+                    x-on:click="showTagInput = false"
+                    class="inline-flex items-center px-4 py-2 bg-gray-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-400 focus:bg-gray-400 active:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     Cancel
                 </button>
             </form>
